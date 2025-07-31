29f5eaf12d2a144256bedc357cf23a31d56c0490
reshm <reshmapsuresh20@gmail.com>
2025-07-31 20:18:59 +0530

    refactor : Add Discount enum to calculate the discount and extracted the method for better readability

--- src/main/java/com/bnpp/katas/developmentbooks/service/CalculateBookPriceService.java
+++ src/main/java/com/bnpp/katas/developmentbooks/service/CalculateBookPriceService.java
@@ -2,6 +2,7 @@ package com.bnpp.katas.developmentbooks.service;
 
 import com.bnpp.katas.developmentbooks.model.BookRequest;
 import com.bnpp.katas.developmentbooks.store.BooksEnum;
+import com.bnpp.katas.developmentbooks.store.DiscountEnum;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -14,16 +15,10 @@ public class CalculateBookPriceService {
         double totalPrice = 0.0;
         Map<BooksEnum, Integer> bookCountMap = getBookCounts(bookRequest);
 
-        Map<BooksEnum, Integer> bookCountsCopy = new HashMap<> (bookCountMap);
-        while ((bookCountsCopy.values().stream().anyMatch(count -> count > 0))) {
+        Map<BooksEnum, Integer> booksCount = new HashMap<> (bookCountMap);
+        while (hasBooksLeft(booksCount)) {
 
-            List<BooksEnum> selectedBooks = new ArrayList<>();
-            for (BooksEnum bookEnum : BooksEnum.values()) {
-                if (bookCountsCopy.getOrDefault(bookEnum, 0) > 0) {
-                    selectedBooks.add(bookEnum);
-                    bookCountsCopy.put(bookEnum, bookCountsCopy.get(bookEnum) - 1);
-                }
-            }
+            List<BooksEnum> selectedBooks = selectBooks(booksCount);
             if (!selectedBooks.isEmpty()) {
                 double discount = getDiscount(selectedBooks.size());
                 double actualPrice = selectedBooks.size() * 50;
@@ -31,6 +26,22 @@ public class CalculateBookPriceService {
             }
         }
         return totalPrice;
+    }
+
+    private List<BooksEnum> selectBooks(Map<BooksEnum, Integer> booksCount) {
+        List<BooksEnum> selectedBooks = new ArrayList<>();
+        for (BooksEnum bookEnum : BooksEnum.values()) {
+            if (booksCount.getOrDefault(bookEnum, 0) > 0) {
+                selectedBooks.add(bookEnum);
+                booksCount.put(bookEnum, booksCount.get(bookEnum) - 1);
+            }
+        }
+        return selectedBooks;
+    }
+
+
+    private boolean hasBooksLeft(Map<BooksEnum, Integer> booksCount) {
+        return booksCount.values().stream().anyMatch(count -> count > 0);
     }
 
     private Map<BooksEnum, Integer> getBookCounts(List<BookRequest> bookRequest) {
@@ -44,13 +55,9 @@ public class CalculateBookPriceService {
         return bookCounts;
     }
 
-    private double getDiscount(int bookCount) {
-        if (bookCount == 3) {
-            return 0.10;
-        } else if (bookCount == 2) {
-            return 0.05;
-        } else {
-            return 0.0;
-        }
+    private double getDiscount(int uniqueBookCount) {
+        return Arrays.stream(DiscountEnum.values())
+                .filter(discount -> discount.getNumberOfDistinctItems() == uniqueBookCount)
+                .map(DiscountEnum::getDiscountPercentage).findFirst().orElse(0.0);
     }
 }
---
+++ src/main/java/com/bnpp/katas/developmentbooks/store/DiscountEnum.java
@@ -0,0 +1,19 @@
+package com.bnpp.katas.developmentbooks.store;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum DiscountEnum {
+
+	TWO_BOOKS(2, 0.05), 
+	THREE_BOOKS(3, 0.10), 
+	FOUR_BOOKS(4, 0.20), 
+	FIVE_BOOKS(5, 0.25);
+	
+	private final int numberOfDistinctItems;
+    private final double discountPercentage;
+    
+	public static final double PRICE = 50.0;
+}
