ffb4627f1cd010864f62c76483de67e61f31a1b0
reshm <reshmapsuresh20@gmail.com>
2025-07-31 19:52:20 +0530

    feat : Two different books purchase gave a 5% discount

--- src/test/java/com/bnpp/katas/developmentbooks/service/CalculateBookPriceServiceTest.java
+++ src/test/java/com/bnpp/katas/developmentbooks/service/CalculateBookPriceServiceTest.java
@@ -1,6 +1,7 @@
 package com.bnpp.katas.developmentbooks.service;
 
 import com.bnpp.katas.developmentbooks.model.BookRequest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +18,7 @@ public class CalculateBookPriceServiceTest {
     private CalculateBookPriceService calculateBookPriceService;
 
     @Test
+    @DisplayName ("Single book purchase : book price will be the same")
     public void calculatePriceForASingleBookPurchase_ShouldReturnPriceFifty() {
 
         List<BookRequest> bookRequests = Arrays.asList(new BookRequest(1, 1));
@@ -24,5 +26,15 @@ public class CalculateBookPriceServiceTest {
         double price = calculateBookPriceService.calculatePrice(bookRequests);
 
         assertEquals(50.0, price);
+    }
+    @Test
+    @DisplayName ("Two different book purchase : 5% discount is applicable")
+    public void calculatePriceForTwoDifferentBookPurchase_ShouldApplyFivePercentDiscount() {
+
+        List<BookRequest> bookRequests = Arrays.asList(new BookRequest(1, 1),new BookRequest(2, 1));
+
+        double price = calculateBookPriceService.calculatePrice(bookRequests);
+
+        assertEquals(95.0, price);
     }
 }
