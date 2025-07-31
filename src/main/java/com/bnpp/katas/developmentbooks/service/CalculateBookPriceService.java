a902060ed70a13323a98842a7866e7a27d1bf1a2
reshm <reshmapsuresh20@gmail.com>
2025-07-31 19:45:22 +0530

    chore : Add yaml file and Openapi dependencies for Generating model

--- src/main/java/com/bnpp/katas/developmentbooks/service/CalculateBookPriceService.java
+++ src/main/java/com/bnpp/katas/developmentbooks/service/CalculateBookPriceService.java
@@ -1,17 +1,22 @@
 package com.bnpp.katas.developmentbooks.service;
 
+import com.bnpp.katas.developmentbooks.model.BookRequest;
 import com.bnpp.katas.developmentbooks.store.BooksEnum;
-import java.util.Arrays;
+import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+
+@Service
 public class CalculateBookPriceService {
 
-    public double calculatePrice(int bookId, int quantity) {
+    public double calculatePrice(List<BookRequest> bookRequest) {
 
-        BooksEnum book = Arrays.stream(BooksEnum.values())
-                .filter(b -> b.getId() == bookId)
-                .findFirst()
-                .orElse(null);
-
-        return book.getPrice() * quantity;
+        return bookRequest.stream().mapToDouble(bookReq -> {
+            BooksEnum book = Arrays.stream(BooksEnum.values())
+                    .filter(b -> b.getId() == bookReq.getId()).findFirst()
+                    .orElse(null);
+            return (book != null) ? book.getPrice() * bookReq.getQuantity() : 0.0;
+        }).sum();
     }
 }
--- src/test/java/com/bnpp/katas/developmentbooks/service/CalculateBookPriceServiceTest.java
+++ src/test/java/com/bnpp/katas/developmentbooks/service/CalculateBookPriceServiceTest.java
@@ -1,18 +1,27 @@
 package com.bnpp.katas.developmentbooks.service;
 
+import com.bnpp.katas.developmentbooks.model.BookRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class CalculateBookPriceServiceTest {
 
+    @Autowired
+    private CalculateBookPriceService calculateBookPriceService;
+
     @Test
     public void calculatePriceForASingleBookPurchase_ShouldReturnPriceFifty() {
 
-        CalculateBookPriceService calculateBookPriceService = new CalculateBookPriceService();
+        List<BookRequest> bookRequests = Arrays.asList(new BookRequest(1, 1));
 
-        double price = calculateBookPriceService.calculatePrice(1,1);
+        double price = calculateBookPriceService.calculatePrice(bookRequests);
 
         assertEquals(50.0, price);
     }
