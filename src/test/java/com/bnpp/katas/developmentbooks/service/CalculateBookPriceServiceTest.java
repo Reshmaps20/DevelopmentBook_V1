0977669eb2da5215039d6c9b6d9dace6adad6677
reshm <reshmapsuresh20@gmail.com>
2025-08-01 18:13:04 +0530

    chore : Add pitest plugin for mutation testing ; Update test case for coverage

--- src/test/java/com/bnpp/katas/developmentbooks/service/CalculateBookPriceServiceTest.java
+++ src/test/java/com/bnpp/katas/developmentbooks/service/CalculateBookPriceServiceTest.java
@@ -1,16 +1,17 @@
 package com.bnpp.katas.developmentbooks.service;
 
+import com.bnpp.katas.developmentbooks.exception.InvalidBookException;
+import com.bnpp.katas.developmentbooks.model.BookGroup;
 import com.bnpp.katas.developmentbooks.model.BookRequest;
 import com.bnpp.katas.developmentbooks.model.BookResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class CalculateBookPriceServiceTest {
@@ -97,5 +98,44 @@ public class CalculateBookPriceServiceTest {
         BookResponse price = calculateBookPriceService.calculatePrice(bookRequests);
 
         assertEquals(320.0, price.getFinalPrice ());
+    }
+
+    @Test
+    @DisplayName ("Calculate actual price , total discount and total price")
+    public void calculatePriceForDistinctBooks_ShouldCalculateFinalPriceAndDiscount() {
+
+        List<BookRequest> bookRequests = Arrays.asList(new BookRequest(1, 1),new BookRequest(2, 1));
+
+        BookResponse price = calculateBookPriceService.calculatePrice(bookRequests);
+
+        assertEquals(100.0, price.getActualPrice ());
+        assertEquals(5.0, price.getTotalDiscount ());
+        assertEquals(95.0, price.getFinalPrice ());
+        assertEquals(2, price.getListOfBookGroups ().get (0).getNumberOfBooks ());
+        assertEquals(5.0, price.getListOfBookGroups ().get (0).getDiscountAmount ());
+    }
+
+    @Test
+    @DisplayName ("Calculate each set of book")
+    public void calculatePriceForDistinctBookSet_ShouldCalculatePriceAndDiscount() {
+
+        List<BookRequest> bookRequests = Arrays.asList(new BookRequest(1, 1),new BookRequest(2, 1));
+
+        BookResponse price = calculateBookPriceService.calculatePrice(bookRequests);
+        BookGroup group = price.getListOfBookGroups().get(0);
+
+        assertEquals(2, group.getNumberOfBooks ());
+        assertEquals(100.0, group.getActualPrice());
+        assertEquals(5.0, group.getDiscountAmount());
+        assertEquals(5.0, group.getDiscountPercentage());
+        assertEquals(95.0, group.getFinalGroupPrice());
+    }
+    @Test
+    @DisplayName ("Invalid Book ID should throw exception")
+    public void calculatePrice_withInvalidBookId_shouldThrowException() {
+
+        List<BookRequest> invalidBookId = Arrays.asList(new BookRequest(99, 1));
+
+        assertThrows(InvalidBookException.class, () -> calculateBookPriceService.calculatePrice (invalidBookId));
     }
 }
